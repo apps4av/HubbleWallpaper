93b77b251a67c539a8a6bc1eec5cbf883cb4c11a
apps4av <apps4av@gmail.com>
2018-01-06 15:52:44 -0500

    removed background process. rel 2

--- app/src/main/java/org/apps4av/hubblewallpaper/MainService.java
+++ app/src/main/java/org/apps4av/hubblewallpaper/MainService.java
@@ -2,18 +2,23 @@ package org.apps4av.hubblewallpaper;
 
 import android.app.Service;
 import android.app.WallpaperManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.DisplayMetrics;
-
-import java.util.Timer;
-import java.util.TimerTask;
+import android.util.Log;
 
 /**
  * Created by zkhan on 1/1/18.
@@ -22,10 +27,6 @@ import java.util.TimerTask;
 public class MainService extends Service {
 
     private WallpaperManager mWallpaperManager;
-    private Timer mTimer;
-
-    // update time
-    private static final int ONE_HOUR = 60 * 60 * 1000;
 
     private Bitmap mScreenBitmap;
 
@@ -34,17 +35,15 @@ public class MainService extends Service {
         return null;
     }
 
-
     public void onDestroy() {
         super.onDestroy();
+        mHandler.removeMessages(0);
         mScreenBitmap.recycle();
-
-        mTimer.cancel();
     }
 
     @Override
-    public int onStartCommand (Intent intent, int flags, int startId) {
-        super.onStartCommand(intent, flags, startId);
+    public void onCreate() {
+        super.onCreate();
 
         mWallpaperManager
                 = WallpaperManager.getInstance(getApplicationContext());
@@ -57,70 +56,101 @@ public class MainService extends Service {
         double hs = ws * dm.heightPixels / dm.widthPixels;
         mScreenBitmap = Bitmap.createBitmap((int)ws, (int)hs, Bitmap.Config.RGB_565);
 
+        // Download when screen turns on.
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                // Screen on. Download
+                NetworkInfo networkInfo = ((ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE)).
+                        getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+                if(!networkInfo.isConnected()) {
+                    // Only on WIFI connected
+                    return;
+                }
+                mHandler.removeMessages(0);
+                mHandler.sendEmptyMessage(0);
+            }
+        }, new IntentFilter(Intent.ACTION_SCREEN_ON));
 
         // start
-        WallpaperTask task = new WallpaperTask();
-        mTimer = new Timer();
-        mTimer.scheduleAtFixedRate(task, 0, ONE_HOUR);
+        mHandler.sendEmptyMessage(0);
 
+    }
 
+    @Override
+    public int onStartCommand (Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
         return START_STICKY;
     }
 
+    /**
+     * Does everything in background
+     */
+    private void performChange() {
 
-    private class WallpaperTask extends TimerTask {
-        public void run() {
-            // download / parse
+        StreamHelper.insert(getApplicationContext(), StreamParser.parse(getApplicationContext()));
 
-            StreamHelper.insert(getApplicationContext(), StreamParser.parse(getApplicationContext()));
+        // query and set
+        Bitmap b = StreamHelper.get(getApplicationContext());
+        if(null == b) {
+            // default when nothing
+            b = BitmapUtils.getDefaultImage(getApplicationContext());
+        }
 
-            // query and set
-            Bitmap b = StreamHelper.get(getApplicationContext());
-            if(null == b) {
-                // default when nothing
-                b = BitmapUtils.getDefaultImage(getApplicationContext());
-            }
+        // clear
+        mScreenBitmap.eraseColor(0);
 
-            // clear
-            mScreenBitmap.eraseColor(0);
+        // find screen size
+        double ws = mScreenBitmap.getWidth();
+        double hs = mScreenBitmap.getHeight();
+        // find image size
+        double hi = b.getHeight();
+        double wi = b.getWidth();
 
-            // find screen size
-            double ws = mScreenBitmap.getWidth();
-            double hs = mScreenBitmap.getHeight();
-            // find image size
-            double hi = b.getHeight();
-            double wi = b.getWidth();
+        // find ratio
+        double rs = ws / hs;
+        double ri = wi / hi;
 
-            // find ratio
-            double rs = ws / hs;
-            double ri = wi / hi;
+        // scale to fit
+        int w;
+        int h;
+        if(rs > ri) {
+            w = (int)(wi * hs/hi);
+            h = (int)hs;
+        }
+        else {
+            w = (int)ws;
+            h = (int)(hi * ws / wi);
+        }
 
-            // scale to fit
-            int w;
-            int h;
-            if(rs > ri) {
-                w = (int)(wi * hs/hi);
-                h = (int)hs;
-            }
-            else {
-                w = (int)ws;
-                h = (int)(hi * ws / wi);
-            }
+        // center
+        int top = (int)(hs - h) / 2;
+        int left = (int)(ws - w) / 2;
 
-            // center
-            int top = (int)(hs - h) / 2;
-            int left = (int)(ws - w) / 2;
+        Canvas c = new Canvas(mScreenBitmap);
+        c.drawBitmap(b, new Rect(0, 0, (int)wi, (int)hi), new Rect(left, top, left + w, top + h), new Paint());
 
-            Canvas c = new Canvas(mScreenBitmap);
-            c.drawBitmap(b, new Rect(0, 0, (int)wi, (int)hi), new Rect(left, top, left + w, top + h), new Paint());
-
-            // set
-            try {
-                mWallpaperManager.setBitmap(mScreenBitmap);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        // set
+        try {
+            mWallpaperManager.setBitmap(mScreenBitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
+    // A way to start refresh thread
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            new Thread() {
+                @Override
+                public void run() {
+                    super.run();
+                    performChange();
+                }
+            }.start();
+        }
+    };
 
 }
